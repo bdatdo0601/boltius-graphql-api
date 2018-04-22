720b933feed14eac2d3e043b940bf6db484c3455
Dat Bac Do <dat.b.do@gmail.com>
2018-04-22 15:49:48 -0400

    add confidence for config

---
+++ config.js
@@ -0,0 +1,24 @@
+import Confidence from "confidence";
+
+const document = Object.freeze({
+    logger: {
+        $filter: "env",
+        production: false,
+        development: true,
+        $default: true,
+    },
+});
+
+const store = new Confidence.Store();
+
+store.load(document);
+
+const criteria = Object.freeze({
+    env: process.env.NODE_ENV,
+});
+
+const retrieve = key => store.get(key, criteria);
+
+export default {
+    retrieve,
+};
--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-import fastify from "fastify";
+import Fastify from "fastify";
+import split from "split2";
+import dotenv from "dotenv";
 
-const server = fastify();
+import configStore from "./config";
+
+dotenv.config();
+
+const fastify = Fastify({
+    logger: configStore.retrieve("/logger"),
+});
+
+fastify.get("/", async (request, reply) => {
+    reply.type("application/json").code(200);
+    return { hello: "worsld" };
+});
+
+// Run the server!
+const start = async () => {
+    try {
+        await fastify.listen(3000);
+        fastify.log.info(`server listening on ${fastify.server.address().port}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+start();
