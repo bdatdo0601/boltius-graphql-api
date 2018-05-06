1be9d34654bab61b77f3ecb8153133caef0bfd1a
Dat Bac Do <dat.b.do@gmail.com>
2018-05-06 13:55:32 -0400

    add admin field

--- src/db/models/Account/user.js
+++ src/db/models/Account/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema(
         username: { type: String, required: true, unique: true },
         email: { type: String, required: true },
         password: { type: String, required: true },
+        isAdmin: { type: Boolean, required: true },
         joinedDate: Date,
         status: String,
         role: roleSchema,
